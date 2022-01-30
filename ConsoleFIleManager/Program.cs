8916b0ed3371eeb77465bed8da502ef85a02f09e
repin <sasharepin@Mail.ru>
2022-01-30 20:16:25 +0300

    Finish Func Copy

--- ConsoleFIleManager/Program.cs
+++ ConsoleFIleManager/Program.cs
@@ -9,21 +9,17 @@ namespace ConsoleFIleManager
 
         static void Main(string[] args)
         {
-            /*            var pathTree = Directory.EnumerateFileSystemEntries(@"C:\test\source", "", SearchOption.AllDirectories);
-                        foreach (string k in pathTree)
-                        {
-                            Console.WriteLine(k);
-                        }*/
+/*            string r = @"C:\test\target";
+            string k = "Kakaka";
+            string f = Path.Combine(r, k);
+            Directory.CreateDirectory(f);
+            return;*/
 
             FO foo = new FO();
             while (foo.exit == false)
             {
                 string command = Console.ReadLine();
                 foo.CommandRead(command);
-                foreach(string p in foo.lstDir)
-                {
-                    Console.WriteLine(p);
-                }
             }
         }
     }
